eefb828d06c307de7e9cede81f31e884e20f4c25
q-Domovoy <ms.titoff1011@yandex.ru>
2023-01-14 22:15:29 +0300

    123

--- main.py
+++ main.py
@@ -1,19 +1,57 @@
 import csv
 import sys
 
-from PyQt5.uic import loadUi
+from PyQt5 import uic
 from PyQt5.QtCore import QDate, Qt
 from PyQt5.QtWidgets import *
 from pyqtgraph import PlotWidget
 
+
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi('MainWindow.ui', self)
-        self.tableWidget.setColumnWidth(0, 300)
-        self.tableWidget.setColumnWidth(1, 300)
-        self.tableWidget.setColumnWidth(2, 300)
-        self.tableWidget.setColumnWidth(3, 300)
+        uic.loadUi('MainWindow.ui', self)
+        self.pushButton.clicked.connect(self.to_table)
+
+    def to_table(self):
+        self.t = TableWindow()
+        self.t.show()
+        self.hide()
+
+
+class TableWindow(MainWindow):
+    def __init__(self):
+        super(TableWindow, self).__init__()
+        uic.loadUi('table.ui', self)
+        self.tableWidget.insertRow(0)
+        self.tableWidget.insertRow(0)
+        self.MenuButton.clicked.connect(self.to_main_menu)
+        self.NewRaceButton.clicked.connect(self.create_new_race)
+
+    def to_main_menu(self):
+        ex.show()
+        self.hide()
+
+    def create_new_race(self):
+        id_race = QTableWidget.currentRow(self.tableWidget)
+        print(id_race)
+        new_race_data = NewRaceDialog()
+        if new_race_data.exec():
+            title = new_race_data.title_line.text()
+            date = new_race_data.date_line.text()
+            organizer = new_race_data.org_line.text()
+            place = new_race_data.place_line.text()
+            type = new_race_data.type_line.text()
+            print(title, date, organizer, place, type)
+        else:
+            print(0)
+
+
+class NewRaceDialog(QDialog):
+    def __init__(self):
+        super(NewRaceDialog, self).__init__()
+        uic.loadUi('NewRace.ui', self)
+
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
